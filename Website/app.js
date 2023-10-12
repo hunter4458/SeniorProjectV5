9b2f88d5e3fa397b55d08ba5ac786e8aee49c27b
hunter4458 <campbellhammer44@gmail.com>
2023-10-11 20:55:12 -0400

    Update app.js

--- Website/app.js
+++ Website/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = 80;
+const PORT = process.env.PORT || 80;
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
