f3c829c3be2edb7ff8e82bcb199dbbef0806fc84
hunter4458 <campbellhammer44@gmail.com>
2023-10-11 22:05:41 -0400

    Final Prototype 2

--- Website/app.js
+++ Website/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 80;
+
+app.use(bodyParser.json());
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,10 +15,34 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/api', (req, res) => {
-    console.log(process.env.OPENAI_API_KEY);
-    res.send('My api key is: ' + process.env.OPENAI_API_KEY);
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
+
+app.get('/loginpage', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'loginpage.html'));
+});
+
+app.post('/chatGPTRequest', (req, res) => {
+    fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + process.env.OPENAI_API_KEY, // Replace with your actual API key
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(req.body),
+    })
+        .then(response => response.json())
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'Failed to fetch data from other service' });
+        });
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
